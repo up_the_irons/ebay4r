ddacfe392becfd652c8b882981da50c54d49b66f
Garry Dolley <gdolley@arpnetworks.com>
2005-12-08 06:29:26 +0000

    Added comments

--- examples/myCredentials.rb
+++ examples/myCredentials.rb
@@ -1,4 +1,6 @@
-$appId     = ''
-$devId     = ''
-$certId    = ''
-$authToken = ''
+# Fill these in before running the examples:
+
+$appId     = 'my_app_id'
+$devId     = 'my_dev_id'
+$certId    = 'my_cert_id'
+$authToken = 'my_auth_token'
