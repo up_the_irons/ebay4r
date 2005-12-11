4f2deaa79f97fb0fb3430503ddee498fedb9d9d9
Garry Dolley <gdolley@arpnetworks.com>
2005-12-11 11:10:22 +0000

    Start of test case relating to eBay Items

---
+++ test/tc_items.rb
@@ -0,0 +1,33 @@
+#!/usr/bin/env ruby
+
+#--
+#
+# Copyright (c) 2005 Garry C. Dolley
+#
+# This file is part of eBay4R.
+#
+# eBay4R is free software; you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 2 of the License, or (at your option) any later 
+# version.
+#
+# eBay4R is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+# 
+# You should have received a copy of the GNU General Public License along with
+# eBay4R; if not, write to the Free Software Foundation, Inc., 51 Franklin
+# Street, Fifth Floor, Boston, MA  02110-1301, USA
+#
+#++
+
+$:.unshift File.join(File.dirname(__FILE__), "..", "lib")
+
+require 'test/unit'
+require 'eBayAPI'
+
+class TestItems < Test::Unit::TestCase
+
+
+end
