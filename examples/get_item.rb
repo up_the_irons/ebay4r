cb569c29b9526e00eb44f39e078f6aff9b21357f
Garry Dolley <gdolley@arpnetworks.com>
2006-01-07 01:29:55 +0000

    Updated w/ new Item #

--- examples/get_item.rb
+++ examples/get_item.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-# $Id: get_item.rb,v 1.5 2005/12/27 01:17:01 garrydolley Exp $
+# $Id: get_item.rb,v 1.6 2006/01/07 01:29:55 garrydolley Exp $
 
 $:.unshift File.join(File.dirname(__FILE__), "..", "lib")
 require 'eBayAPI'
@@ -9,7 +9,7 @@ load('myCredentials.rb')
 eBay = EBay::API.new($authToken, $devId, $appId, $certId, :sandbox => true)
 
 # Replace ItemID with some item you know exists
-resp = eBay.GetItem(:DetailLevel => 'ReturnAll', :ItemID => '4503432058', :IncludeWatchCount => 'true')
+resp = eBay.GetItem(:DetailLevel => 'ReturnAll', :ItemID => '4504390345', :IncludeWatchCount => 'true')
 
 puts "AutoPay: "  + resp.item.autoPay
 puts "Country: "  + resp.item.country
